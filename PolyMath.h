c0cfb95b1e22df85da6139c36a5d2cb6b5a5c5bd
Wiktor Czernik <git@czernik.dev>
2022-09-09 21:41:56 +0200

    Init

---
+++ PolyMath.h
@@ -0,0 +1,13 @@
+#pragma once
+struct Vector2 {
+	int x;
+	int y;
+	Vector2() {
+		x = 0;
+		y = 0;
+	}
+	Vector2(int X, int Y) {
+		x = X;
+		y = Y;
+	}
+};
---
+++ game.cpp
@@ -0,0 +1,87 @@
+
+
+#include "Framework.h"
+#include "PolyMath.h"
+#include <iostream>
+#include <regex>
+using namespace std;
+
+/* Test Framework realization */
+class MyFramework : public Framework {
+private:
+	Vector2 targetScreenSize;
+	Sprite* blueBlock;
+public:
+	virtual void PreInit(int& width, int& height, bool& fullscreen)
+	{
+		width = targetScreenSize.x;
+		height = targetScreenSize.y;
+		fullscreen = false;
+	}
+
+	virtual bool Init() {
+
+		blueBlock = createSprite("E:/dragonik/2022_win64/data/01-Breakout-Tiles.jpg");
+		return true;
+	}
+
+	virtual void Close() {
+
+	}
+
+	virtual bool Tick() {
+		//Sprite* sprait = createSprite("E:/dragonik/2022_win64/data/01-Breakout-Tiles.jpg");
+        drawTestBackground();
+		setSpriteSize(blueBlock, 50, 50);
+		drawSprite(blueBlock, 0, 0);
+		return false;
+	}
+
+	virtual void onMouseMove(int x, int y, int xrelative, int yrelative) {
+
+	}
+
+	virtual void onMouseButtonClick(FRMouseButton button, bool isReleased) {
+
+	}
+
+	virtual void onKeyPressed(FRKey k) {
+	}
+
+	virtual void onKeyReleased(FRKey k) {
+	}
+	
+	virtual const char* GetTitle() override
+	{
+		return "Arcanoid";
+	}
+	MyFramework(Vector2 screenSize) {
+		targetScreenSize = screenSize;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Vector2 defaultScreenSize = Vector2(800, 600);
+	Vector2 screenSize = defaultScreenSize;
+	string command;
+	switch (argc)
+	{
+	case 1:
+		break;
+	case 4:
+		command = argv[1];
+		if (command == "-window") {
+			try {
+				screenSize = Vector2(stoi(argv[2]), stoi(argv[3]));
+				break;
+			}
+			catch (exception e) {cout << e.what(); }
+		}
+	default:
+		cout << "Wrong command line syntax. Usage: \"polybroid.exe -window <width> <height>\"" << endl;
+		break;
+		
+	}
+	return run(new MyFramework(screenSize));
+}
